eea1cb0eb105768890c3f4390277178a84df2570
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2025-10-15 10:00:50 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,9 @@
+let countdown = 5
+let counter = countdown
+for (let index = 0; index < countdown + 1; index++) {
+    basic.showNumber(counter)
+    counter += -1
+}
+basic.forever(function () {
+	
+})
